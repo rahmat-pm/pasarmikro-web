d268c39bbdf8c0277810b4a5ed7c8eb3944124a6
rahmat-pm <rahmat@pasarmikro.id>
2025-07-24 10:49:16 +0700

    fix carousel on light and dark background

---
+++ assets/js/carousel/init-swiper.js
@@ -0,0 +1,43 @@
+function renderSwiper() {
+      const heroSwiper = new Swiper('.hero-swiper', {
+        loop: true,
+        autoplay: {
+          delay: 100,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        effect: 'fade',
+        on: {
+            slideChangeTransitionEnd: function () {
+                const activeSlide = this.slides[this.activeIndex];
+                const theme = activeSlide.getAttribute('data-header-theme') || 'dark';
+
+                const header = document.querySelector('#header');
+                const logoImg = document.querySelector('#logo-img');
+
+                header.classList.remove('light-theme', 'dark-theme');
+                header.classList.add(theme + '-theme');
+
+                if (theme === 'light') {
+                logoImg.src = 'assets/img/logo-horizontal-black.png';
+                } else {
+                logoImg.src = 'assets/img/logo-horizontal-white.png';
+                }
+
+                // 🔁 Change autoplay delay after first run
+                if (this.autoplay && this.autoplay.running && this.params.autoplay.delay === 100) {
+                this.params.autoplay.delay = 5000;
+                this.autoplay.stop();
+                this.autoplay.start();
+                }
+            }
+        }
+      });
+    }
