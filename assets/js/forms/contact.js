080635738de00308765d11dcc05265ca9a35457b
rahmat-pm <rahmat@pasarmikro.id>
2025-07-25 11:08:52 +0700

    add reCaptcha on contact

--- assets/js/forms/contact.js
+++ assets/js/forms/contact.js
@@ -1,40 +1,53 @@
 function contactUs() {
-    const formElement = document.getElementById('contact-form');
-  
-    formElement.addEventListener('submit', async function (event) {
-      event.preventDefault();
-        try {
-          display('submit-button', 'none');
-          display('loading', 'block');
-  
-          const formData = new FormData(formElement);
-          const serializedForm = Object.fromEntries(formData);
+  const formElement = document.getElementById('contact-form');
 
-          const url = `${API_URL}?api_key=${API_TOKEN}&route=contact`;
+  formElement.addEventListener('submit', async function (event) {
+    event.preventDefault();
 
-          const response = await fetch(url, {
-            redirect: "follow",
-            method: 'POST',
-            body: JSON.stringify(serializedForm),
-            headers: {
-              'Content-type': 'text/plain;charset=utf-8'
-            }
-          });
-  
-          if (response.ok) {
-            await response.json();
-            notification('success', "Sukses!", "Terimakasih. Data anda sudah berhasil kami terima. Tim kami akan segera menghubungi anda.");
-            display('submit-button', 'block');
-            display('loading', 'none');
-            formElement.reset();
-          } else {
-            notification('error', "Oops!", "Terjadi kesalahan, silahkan coba lagi.");
-            display('submit-button', 'block');
-          }
-        } catch (error) {
-          console.warn(error);
-          notification('error', "Oops!", "Terjadi kesalahan, silahkan coba lagi.");
-          display('submit-button', 'block');
+    try {
+      display('submit-button', 'none');
+      display('loading', 'block');
+
+      const recaptchaToken = grecaptcha.getResponse();
+      if (!recaptchaToken) {
+        notification('error', "Oops!", "Please verify the captcha first.");
+        display('submit-button', 'block');
+        display('loading', 'none');
+        return;
+      }
+
+      const formData = new FormData(formElement);
+      const serializedForm = Object.fromEntries(formData);
+      serializedForm['recaptcha_token'] = recaptchaToken;
+
+      const url = `${API_URL}?api_key=${API_TOKEN}&route=contact`;
+
+      const response = await fetch(url, {
+        redirect: "follow",
+        method: 'POST',
+        body: JSON.stringify(serializedForm),
+        headers: {
+          'Content-type': 'text/plain;charset=utf-8'
         }
-    });
-  }
+      });
+
+      if (response.ok) {
+        await response.json();
+        notification('success', "Success!", "Thank you. We have successfully received your data. Our team will contact you soon..");
+        formElement.reset();
+        grecaptcha.reset(); // Reset the captcha
+      } else {
+        notification('error', "Oops!", "An error occurred, please try again..");
+      }
+
+      display('submit-button', 'block');
+      display('loading', 'none');
+
+    } catch (error) {
+      console.warn(error);
+      notification('error', "Oops!", "An error occurred, please try again..");
+      display('submit-button', 'block');
+      display('loading', 'none');
+    }
+  });
+}
