07911697482c3928967718eaa4d1853e15c4b471
rahmat-pm <rahmat@pasarmikro.id>
2025-07-25 12:03:56 +0700

    fixing recaptcha not detected

--- assets/js/forms/contact.js
+++ assets/js/forms/contact.js
@@ -1,47 +1,45 @@
 function contactUs() {
   const formElement = document.getElementById('contact-form');
 
-  formElement.addEventListener('submit', async function (event) {
+  formElement.addEventListener('submit', function (event) {
     event.preventDefault();
 
     display('submit-button', 'none');
     display('loading', 'block');
 
-    try {
-      // Get reCAPTCHA token just before sending the form
-      const token = await grecaptcha.execute('6LcXU44rAAAAAL6l6FikP4IUFm_Y3CNtzmtgxMfB', { action: 'submit' });
+    // Wait until reCAPTCHA is ready
+    grecaptcha.ready(async function () {
+      try {
+        // Get reCAPTCHA token
+        const token = await grecaptcha.execute('6LcXU44rAAAAAL6l6FikP4IUFm_Y3CNtzmtgxMfB', { action: 'submit' });
 
-      // Add token to form data
-      const formData = new FormData(formElement);
-      formData.append('recaptcha_token', token);
-      const serializedForm = Object.fromEntries(formData.entries());
+        // Prepare FormData
+        const formData = new FormData(formElement);
+        formData.append('recaptcha_token', token);
 
-      const url = `${API_URL}?api_key=${API_TOKEN}&route=contact`;
+        const url = `${API_URL}?api_key=${API_TOKEN}&route=contact`;
 
-      const response = await fetch(url, {
-        method: 'POST',
-        redirect: 'follow',
-        body: JSON.stringify(serializedForm),
-        headers: {
-          'Content-type': 'text/plain;charset=utf-8'
+        const response = await fetch(url, {
+          method: 'POST',
+          body: formData
+        });
+
+        const result = await response.json();
+        console.log(result);
+
+        if (result.success) {
+          notification('success', "Success!", "Thank you. We have successfully received your data. Our team will contact you soon.");
+          formElement.reset();
+        } else {
+          notification('error', "Oops!", "An error occurred, please try again.");
         }
-      });
-
-      const result = await response.json();
-      console.log(result)
-
-      if (result.success) {
-        notification('success', "Success!", "Thank you. We have successfully received your data. Our team will contact you soon..");
-        formElement.reset();
-      } else {
+      } catch (error) {
+        console.warn(error);
         notification('error', "Oops!", "An error occurred, please try again.");
       }
-    } catch (error) {
-      console.warn(error);
-      notification('error', "Oops!", "An error occurred, please try again.");
-    }
 
-    display('submit-button', 'block');
-    display('loading', 'none');
+      display('submit-button', 'block');
+      display('loading', 'none');
+    });
   });
 }
